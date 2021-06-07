ff488256cbfca36369321f110895f8da8e13b6e8
Nsovo-Ngobeni <58889409+Nsovo-Ngobeni@users.noreply.github.com>
2021-06-07 17:46:54 +0200

    Added a component to switch the timer mode

--- src/App.tsx
+++ src/App.tsx
@@ -12,45 +12,127 @@ const App = () => {
 					? currentTimeState - 1
 					: currentTimeState
 			);
+			if (currentTimeState === 0) {
+				onTimeout();
+			}
 		}, 1000);
 		return () => clearTimeout(timer);
 	});
 
-	// eslint-disable-next-line
-	const [startTimeState, setStartTimeState] = useState(1500);
+	const [startTimeState, setStartTimeState] = useState(25 * 60);
 
-	const [timerState, setTimerState] = useState('work session');
+	const [timerMode, setTimerMode] = useState('work session');
 
 	const [currentTimeState, setCurrentTimeState] = useState(startTimeState);
 
+	const [setSize] = useState(3);
+
+	const [sessionGoal] = useState(6);
+
 	const [countdownState, setCountdownState] = useState(false);
+
+	const [sessionCounter, setCounterState] = useState(() => {
+		let set = (() => {
+			let temp = [];
+			for (let i = 0; i < setSize; i++) {
+				temp.push(false);
+			}
+			return temp;
+		})();
+		let numOfSets = Math.ceil(sessionGoal / setSize);
+		let temp: boolean[][] = [];
+		for (let i = 0; i < numOfSets; i++) {
+			temp.push([...set]);
+		}
+		return temp;
+	});
 
 	const toggleCountdown = () => {
 		setCountdownState(!countdownState);
 	};
 
-	const switchTimerState = () => {
-		switch (timerState) {
-			case 'work session':
-				setTimerState('short break');
-				setStartTimeState(5 * 60);
-				setCurrentTimeState(5 * 60);
-				break;
-			case 'short break':
-				setTimerState('long break');
-				setStartTimeState(15 * 60);
-				setCurrentTimeState(15 * 60);
-				break;
-			case 'long break':
-				setTimerState('work session');
-				setStartTimeState(25 * 60);
-				setCurrentTimeState(25 * 60);
-				break;
-			default:
-				setTimerState('work session');
-				setStartTimeState(25 * 60);
-				setCurrentTimeState(25 * 60);
-				break;
+	// Triggered when timer hits zero
+	const onTimeout = () => {
+		if (timerMode === 'work session') {
+			incrementSessionCounter();
+		}
+		switchTimerMode(true);
+	};
+
+	const incrementSessionCounter = () => {
+		let tempCounter = [...sessionCounter];
+
+		// Finds the index of the nearest unfilled dot and fills it
+		for (let i = 0; i < sessionCounter.length; i++) {
+			let dotArray = sessionCounter[i];
+			for (let j = 0; j < dotArray.length; j++) {
+				let dot = dotArray[j];
+				if (!dot) {
+					tempCounter[i][j] = true;
+					i = sessionCounter.length;
+					break;
+				}
+			}
+		}
+
+		// Checks if the sessionCounter is full
+		if (
+			sessionCounter[sessionCounter.length - 1][sessionCounter[0].length - 1]
+		) {
+			// Adds another array of dots
+			let appendedDotArray = [];
+			for (let i = 0; i < sessionCounter[0].length; i++) {
+				appendedDotArray.push(false);
+			}
+			tempCounter.push(appendedDotArray);
+		}
+
+		setCounterState(tempCounter);
+	};
+
+	// Switches the current mode of the timer
+	const switchTimerMode = (skipLongBreaks: boolean) => {
+		if (skipLongBreaks) {
+			switch (timerMode) {
+				case 'work session':
+					setTimerMode('short break');
+					setStartTimeState(5 * 60);
+					setCurrentTimeState(5 * 60);
+					break;
+				case 'short break':
+					setTimerMode('work session');
+					setStartTimeState(25 * 60);
+					setCurrentTimeState(25 * 60);
+					break;
+				default:
+					setTimerMode('work session');
+					setStartTimeState(25 * 60);
+					setCurrentTimeState(25 * 60);
+					break;
+			}
+		} else {
+			switch (timerMode) {
+				case 'work session':
+					setTimerMode('short break');
+					setStartTimeState(5 * 60);
+					setCurrentTimeState(5 * 60);
+					break;
+				case 'short break':
+					setTimerMode('long break');
+					setStartTimeState(15 * 60);
+					setCurrentTimeState(15 * 60);
+					break;
+				case 'long break':
+					setTimerMode('work session');
+					setStartTimeState(25 * 60);
+					setCurrentTimeState(25 * 60);
+					break;
+				default:
+					setTimerMode('work session');
+					setStartTimeState(25 * 60);
+					setCurrentTimeState(25 * 60);
+					break;
+			}
 		}
 		setCountdownState(false);
 	};
@@ -67,16 +149,10 @@ const App = () => {
 				currentTime={currentTimeState}
 				toggleCoundown={toggleCountdown}
 			/>
-			<Dots
-				dotArray={[
-					[true, true, true],
-					[true, true, true],
-					[true, false, false],
-				]}
-			/>
+			<Dots dotArray={sessionCounter} />
 			<TimerControl
-				switchTimerState={switchTimerState}
-				currentTimerState={timerState}
+				switchTimerMode={() => switchTimerMode(false)}
+				currentTimerState={timerMode}
 			/>
 		</div>
 	);
--- src/Components/TimerControl.tsx
+++ src/Components/TimerControl.tsx
@@ -1,12 +1,31 @@
+import { CSSProperties } from 'react';
+import { ReactComponent as Icon } from '../Assets/switch-arrows.svg';
 interface timerControlProps {
-	switchTimerState: () => void;
+	switchTimerMode: () => void;
 	currentTimerState: String;
 }
 
 const TimerControl = (props: timerControlProps) => {
+	const containerStyling: CSSProperties = {
+		display: 'flex',
+		margin: 'auto',
+		alignContent: 'center',
+		justifyContent: 'center',
+	};
+
+	const iconStyling: CSSProperties = {
+		width: '30px',
+	};
+
+
 	return (
-		<div className='timer-control-container' onClick={props.switchTimerState}>
-			<p>{`< ${props.currentTimerState.toUpperCase()} >`}</p>
+		<div
+			style={containerStyling}
+			className='timer-control-container'
+			onClick={props.switchTimerMode}
+		>
+			<Icon style={iconStyling} />
+			<p>{props.currentTimerState.toUpperCase()}</p>
 		</div>
 	);
 };
